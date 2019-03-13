8ca9f464745ce30c724e3074d3e78508a34471ba
gomurder <35430198+gomurder@users.noreply.github.com>
2019-03-13 04:32:08 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ var express = require('express');
 var app = express();
 var sunucu = app.listen('4567');
 var io = require('socket.io').listen(sunucu);
+var shortid = require('shortid');
 
 console.log("sunucu aktif");
 var connectedPlayers =0;
 io.on('connection',function(socket){
+
+    var thisClientId = shortid.generate();
+	socket.join(thisClientId);
+	io.to(thisClientId).emit('merhaba',{"id":thisClientId});
+	socket.leave();
+
     connectedPlayers++;
     console.log("Bağlantı Sağlandı, Online " , connectedPlayers, " Kişi var.");
     for(var i = 0; i<1;i++){
@@ -22,9 +29,11 @@ io.on('connection',function(socket){
     socket.on('BallMove',function(data){
         socket.broadcast.emit('BallMoving',data);
     });
-    socket.on('goal',function(data){
-        socket.broadcast.emit('BallMoving',data);
+
+    socket.on('BallTest',function(data){
+        socket.broadcast.emit('testreceive',data);
     });
+
     //  FirstReppingBall
     // BallRep
 
@@ -34,6 +43,7 @@ io.on('connection',function(socket){
 
     socket.on('disconnect',function(cb){
         console.log("birisi çıktı gitti");
+        connectedPlayers --;
     });
 
 });
