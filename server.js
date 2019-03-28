3d9039df1e55aedad0d7c71c64716abc8e54ca08
gomurder <35430198+gomurder@users.noreply.github.com>
2019-03-28 03:11:59 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,31 @@ var app = express();
 var sunucu = app.listen('3000');
 var io = require('socket.io').listen(sunucu);
 var shortid = require('shortid');
+var mysql = require('mysql');
 
 var WaitingRooms = [];
 var PlayingRooms = [];
+
+var connection = mysql.createConnection({
+	host     : '127.0.0.1',
+	user     : 'root',
+	password : '',
+	database : 'static'
+});
+
+var users = {
+    "username":"isframe",
+    "password":"redeneme",
+    "score":10
+}
+
+/*connection.query('INSERT INTO userList SET ?',users,function(error,results,fields){
+    if(error){
+        console.log("anaskm");
+    }
+    console.log("test");
+});*/
+
 
 console.log("sunucu aktif");
 var connectedPlayers =0;
@@ -15,6 +37,32 @@ io.on('connection',function(socket){
 	socket.join(thisClientId);
 	io.to(thisClientId).emit('merhaba',{"id":thisClientId});
     socket.leave();*/
+
+    socket.on('Login',function(data){
+
+        var username = data["username"];
+        var password = data["password"];
+        connection.query('SELECT * FROM userList WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			if (results.length > 0) {
+				console.log("giriş yapıldı");
+			} else {
+				console.log("kullanıcı adı ya da şifre yanlış!");
+			}			
+		});
+    });
+
+    socket.on('UpdateData',function(data){
+        let datas =[data["score"],data["username"]];
+        connection.query('UPDATE userlist SET score = ? WHERE username = ?',datas,function(error,results,fields){
+            if(error){
+                console.log("error");
+            }
+            else{
+                console.log("success! Result: ",results);
+            }
+        });
+
+    });
     
     socket.on('QuickMatch',function(data){
 		//socket.join(data);
@@ -55,6 +103,7 @@ io.on('connection',function(socket){
 
     socket.on('BallMove',function(data){
         socket.broadcast.to(data["from"]).emit('BallMoving',data);
+        console.log("kick data: ",data);
     });
 
     socket.on('BallTest',function(data){
@@ -74,6 +123,7 @@ io.on('connection',function(socket){
 
     socket.on('BallRep',function(data){
         socket.broadcast.to(data["from"]).emit('FirstReppingBall',data);
+        console.log("Collision: ",data);
     });
 
     socket.on('disconnect',function(cb){
